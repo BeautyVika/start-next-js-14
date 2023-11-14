206c8133dcaa7384943f48c75e285f1f48c5254b
BeautyVika <vikachk54@gmail.com>
2023-11-14 11:26:56 +0300

    start project

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const Footer = () => {
+    return (
+        <footer className="container">
+            Created by @Vika P.
+        </footer>
+    );
+};
+
+export {Footer};
